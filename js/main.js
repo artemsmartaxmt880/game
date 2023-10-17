8e17617c9dd0d6cdec9b60938034f2472b92970d
artemsmartaxmt880 <artemsmartaxmt880@mail.ru>
2023-10-17 23:25:15 +0300

    1

--- js/main.js
+++ js/main.js
@@ -3,25 +3,53 @@ function game() {
     let animationId = null;
     let inPause = false;
 
+    const speed = 2;
+
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.tree');
     const playBtn = document.querySelector('.playBtn');
+
+    const treeOne = trees[0];
+    const treesCoords = [];
+    trees.forEach((item) => {
+        coords = getCoordinate(item)
+        treesCoords.push(coords)
+    })
+    const coordTreeOne = getCoordinate(treeOne);
+    function getCoordinate(item) {
+        const coord = window.getComputedStyle(item).top;
+        return parseFloat(coord);
+    }
     animationId = requestAnimationFrame(startGame);
     function startGame() {
         console.log('inProcess');
+        treesAnimation()
         animationId = requestAnimationFrame(startGame);
     }
+    function treesAnimation() {
+        for (let i = 0; i < trees.length; i++) {
+            const tree = trees[i]
+            const coords = treesCoords[i]
 
+            let newCoord = coords + speed;
+            if (newCoord > window.innerHeight) {
+                newCoord = -300;
+            }
+            treesCoords[i]= newCoord;
+            tree.style.top = `${newCoord}px`
+        }
+    }
     playBtn.addEventListener('click', () => {
         let img = document.querySelector('.pause')
         const imgList = ["./img/pause.png", "./img/play.png"]
         if (inPause) {
+            requestAnimationFrame(startGame)
             img.src = imgList[0];
         }
         else {
+            cancelAnimationFrame(animationId)
             img.src = imgList[1];
         }
         inPause = !inPause;
-        cancelAnimationFrame(animationId)
     })
 }
